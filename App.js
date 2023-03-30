e55b771137c1653c3974144328c80fcc4000183d
sumit <sumit@EzAdmins-MacBook-Air.local>
2023-03-30 11:18:08 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import RnKredmint from "react-native-kredmint";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <RnKredmint
+        username={"7017370753"}
+        page={"profile"} // profile & payment
+        environments={"sandbox"} // sandbox & production
+        source={"BNPL"}
+        invoiceNumber={"123431"}
+        paymentDate={1671129000000}
+        amount={11223}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
